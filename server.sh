7b2180262bc9646b54c47a232783c8d6678014ca
renegadespork <jessekilburn@gmail.com>
2024-09-14 05:15:18 -0700

    Removed listener ini flag. Devs fixed the binding bug.

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting server..."
-./server/FactoryServer.sh -log -multihome=0.0.0.0 -ini:Engine:[HTTPServer.Listeners]:DefaultBindAddress=any
+./server/FactoryServer.sh -log -multihome=0.0.0.0
